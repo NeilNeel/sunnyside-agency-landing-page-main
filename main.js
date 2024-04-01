74705677680c73f24e5c7f696cdf3ad4397c1a7a
neel <neelbarvaliya04@gmail.com>
2024-04-01 14:08:28 +0530

    made media query

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".nav--links-container");
+hamburger.addEventListener("click", function (e) {
+  navBar.classList.toggle("active");
+});
